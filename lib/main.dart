988ec71cd07ff1c01bac695d774ff318dfa31a38
Mostafa170420 <mostafa170420@gmail.com>
2024-08-03 10:55:42 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:product_app/view/home/home_view.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const MyApp(), // Wrap your app
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
---
+++ lib/view/splash/splash.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return const Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
